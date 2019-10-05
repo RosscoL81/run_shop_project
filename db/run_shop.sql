3df7ca5ddcd03685555f74185ad715d7527d5792
Ross Lorimer <wross.lorimer@googlemail.com>
2019-10-05 16:41:57 +0100

    run_shop.sql created

---
+++ db/run_shop.sql
@@ -0,0 +1,18 @@
+DROP TABLE trainers;
+DROP TABLE brands;
+
+CREATE TABLE brands (
+  id SERIAL8 primary key,
+  name VARCHAR(255)
+);
+
+CREATE TABLE trainers (
+  id SERIAL8 primary key,
+  trainer_name VARCHAR(255),
+  description TEXT,
+  size INT8,
+  quantity INT8,
+  buy_cost INT8,
+  sell_price INT8,
+  brand_id INT8 REFERENCES brands(id)
+);
